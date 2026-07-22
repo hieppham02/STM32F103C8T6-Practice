415f85021704c9a57b0463ef34414f06703450c3
Hoang Hiep Pham <83917749+hieppham02@users.noreply.github.com>
2026-07-22 22:26:56 +0700

    Delete e0 directory

--- e0/e0/item.cpp
+++
@@ -1,7 +0,0 @@
-#include "lib.h"
-
-uint16_t x = 1000;
-
-void mPrintf() {
-	printf("%d", x);
-}
--- e0/e0/lib.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include <stdio.h>
-#include <stdint.h>
-
-extern uint16_t x;
-void mPrintf();
